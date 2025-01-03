205a73d9dbad4e69fb7850a59a9b4ea98f6caed4
Julia Tuka <julia.tuka@aon.at>
2025-01-03 21:09:00 +0100

    Changed Station structure

--- GameServer/GameServer/Models/Station.cs
+++ GameServer/GameServer/Models/Station.cs
@@ -1,7 +1,17 @@
 ﻿namespace GameServer.Models
 {
-    public class Station : IProperty
-    {
-        public Player? Owner { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-    }
+	public class Station : PropertyField
+	{
+		public int[] RentPrices { get; set; }
+
+		public override void LandOn(Player player)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Pass(Player player)
+		{
+			throw new NotImplementedException();
+		}
+	}
 }
