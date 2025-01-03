03a6f974758610ededf16965dc990b1297aa4377
kochmarvin <marvin.info@gmx.at>
2025-01-03 18:14:04 +0100

    :zap: added auth guard

--- game-angular/src/app/core/auth/auth.guard.ts
+++ game-angular/src/app/core/auth/auth.guard.ts
@@ -1,5 +1,38 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
+import { PacketService } from '../../shared/services/packet/packet.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private packetService: PacketService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.packetService.getConnectionStatus().pipe(
+      take(1),
+      map((isConnected) => {
+        if (isConnected) {
+          return true;
+        }
+        
+        return this.router.createUrlTree(['/login']);
+      })
+    );
+  }
+}
--- game-angular/src/app/shared/services/packet/packet.service.ts
+++ game-angular/src/app/shared/services/packet/packet.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import * as signalR from '@microsoft/signalr';
 import { environment } from '../../../../environments/environment';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PacketService {
+
   private hubConnection: signalR.HubConnection;
+  private connectionStatus = new BehaviorSubject<boolean>(false);
 
   constructor() {
     this.hubConnection = new signalR.HubConnectionBuilder()
@@ -16,19 +18,30 @@ export class PacketService {
   }
 
   startConnection(): Observable<void> {
+    this.hubConnection.onclose(() => {
+      console.warn('SignalR connection closed');
+      this.connectionStatus.next(false);
+    });
+
     return new Observable<void>((observer) => {
       this.hubConnection
         .start()
         .then(() => {
           console.log('Connection established with SignalR hub');
+          this.connectionStatus.next(true);
           observer.next();
           observer.complete();
         })
         .catch((error) => {
+          this.connectionStatus.next(false);
           console.error('Error connecting to SignalR hub:', error);
           observer.error(error);
         });
     });
+  }
+
+  getConnectionStatus(): Observable<boolean> {
+    return this.connectionStatus.asObservable();
   }
 
   receiveMessage(): Observable<string> {
