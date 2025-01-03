0068e1c069f475979faa2f011e7b6fbf4601617e
Julia Tuka <julia.tuka@aon.at>
2025-01-03 18:06:59 +0100

    Added rolled dice action for RollDicePacket

--- GameServer/GameServer/Handlers/PacketHandler.cs
+++ GameServer/GameServer/Handlers/PacketHandler.cs
@@ -8,63 +8,91 @@ using System.Text.Json;
 
 namespace GameServer.Handlers
 {
-    public class PacketHandler
-    {
-        private readonly Dictionary<string, Func<Packet, HubCallerContext, Task>> _packetFunctions = new();
-        private readonly IHubContext<Lobby> _lobbyContext;
-        private readonly ConnectionMapping _connectionMapping;
+	public class PacketHandler
+	{
+		private readonly Dictionary<string, Func<Packet, HubCallerContext, Task>> _packetFunctions = new();
+		private readonly IHubContext<Lobby> _lobbyContext;
+		private readonly ConnectionMapping _connectionMapping;
 
-        public PacketHandler(IHubContext<Lobby> lobbyContext, ConnectionMapping connectionMapping)
-        {
-            _lobbyContext = lobbyContext;
+		public PacketHandler(IHubContext<Lobby> lobbyContext, ConnectionMapping connectionMapping)
+		{
+			_lobbyContext = lobbyContext;
 
-            // Hier ein neues packet registrieren
-            _packetFunctions.Add("SAMPLE", HandleSamplePacket);
-            _packetFunctions.Add("REGISTER", HandleRegisterPacket);
-            _connectionMapping = connectionMapping;
-        }
+			// Hier ein neues packet registrieren
+			_packetFunctions.Add("SAMPLE", HandleSamplePacket);
+			_packetFunctions.Add("REGISTER", HandleRegisterPacket);
+			_packetFunctions.Add("ROLL_DICE", HandleRollDicePacket);
+			_connectionMapping = connectionMapping;
+		}
 
-        public async Task HandleRegisterPacket(Packet packet, HubCallerContext context)
-        {
-            RegisterPacket parsed = (RegisterPacket) packet;
+		private async Task HandleRollDicePacket(Packet packet, HubCallerContext context)
+		{
+			RollDicePacket parsedpacket = (RollDicePacket)packet;
 
-            string playerName = parsed.PlayerName ?? GamertagGenerator.GenerateRandomGamertag();
-            string connectionId = context.ConnectionId;
+			Game game = GetGame(context);
 
-            PlayerStore.Add(playerName, connectionId);
-        }
+			if (game.CurrentMover == null || game.CurrentMover.Name != parsedpacket.PlayerName)
+				return;
 
-        public async Task HandlePacket(Packet packet, HubCallerContext context)
-        {
-            // checken obs das überhaupt gibt 
+			int rolled = game.RollDice();
 
-            await _packetFunctions[packet.Type](packet, context);
-        }
+			RolledPacket rolledPacket = new RolledPacket();
+			rolledPacket.PlayerName = parsedpacket.PlayerName;
+			rolledPacket.RolledNumber = rolled;
 
-        private async Task HandleSamplePacket(Packet packet, HubCallerContext context)
-        {
-            // schauen wer das packet egschickt hat und diesen player getten,
-            // Das ist erlaubt weil kein bock auf visitor pattern
-            SamplePacket parsed = (SamplePacket) packet;
+			string packetJson = JsonSerializer.Serialize(rolledPacket);
+			await _lobbyContext.Clients.Group(GetRoomName(context)).SendAsync("ReceivePacket", packetJson); 
 
-            Game game = GetGame(context);
-            // gane. ... einfach drauf reagieren
+			// game.continue after roll
 
-            string packetJson = JsonSerializer.Serialize(packet);
-            await _lobbyContext.Clients.Group("SAMPLE").SendAsync("ReceivePacket", packetJson);
-        }
+			// move player to new field -> and check if passed Go field
+			// check on ownership of field
+			// if not owned sent packet if they want to buy
+			// if owner exists than check field for current rent -> state game -> houses -> rent? -> pay Player2 so und so viel money 
 
-        private string GetRoomName(HubCallerContext context)
-        {
-            _connectionMapping.TryGetRoomName(context.ConnectionId, out string roomName);
-            return roomName;
-        }
+		}
 
-        private Game GetGame(HubCallerContext context)
-        {
-            // null checks
-            string roomName = GetRoomName(context);
-            return RoomStore.GetGame(roomName);
-        }
-    }
+		public async Task HandleRegisterPacket(Packet packet, HubCallerContext context)
+		{
+			RegisterPacket parsed = (RegisterPacket)packet;
+
+			string playerName = parsed.PlayerName ?? GamertagGenerator.GenerateRandomGamertag();
+			string connectionId = context.ConnectionId;
+
+			PlayerStore.Add(playerName, connectionId);
+		}
+
+		public async Task HandlePacket(Packet packet, HubCallerContext context)
+		{
+			// checken obs das überhaupt gibt 
+
+			await _packetFunctions[packet.Type](packet, context);
+		}
+
+		private async Task HandleSamplePacket(Packet packet, HubCallerContext context)
+		{
+			// schauen wer das packet egschickt hat und diesen player getten,
+			// Das ist erlaubt weil kein bock auf visitor pattern
+			SamplePacket parsed = (SamplePacket)packet;
+
+			Game game = GetGame(context);
+			// gane. ... einfach drauf reagieren
+
+			string packetJson = JsonSerializer.Serialize(packet);
+			await _lobbyContext.Clients.Group("SAMPLE").SendAsync("ReceivePacket", packetJson);
+		}
+
+		private string GetRoomName(HubCallerContext context)
+		{
+			_connectionMapping.TryGetRoomName(context.ConnectionId, out string roomName);
+			return roomName;
+		}
+
+		private Game GetGame(HubCallerContext context)
+		{
+			// null checks
+			string roomName = GetRoomName(context);
+			return RoomStore.GetGame(roomName);
+		}
+	}
 }
