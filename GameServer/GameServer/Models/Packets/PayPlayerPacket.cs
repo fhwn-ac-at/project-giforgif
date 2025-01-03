82284bc32970c864540827996656e16d3dc300ad
Julia Tuka <julia.tuka@aon.at>
2025-01-04 00:09:40 +0100

    Added Pay player packet for paying player that owns field

---
+++ GameServer/GameServer/Models/Packets/PayPlayerPacket.cs
@@ -0,0 +1,13 @@
+﻿namespace GameServer.Models.Packets
+{
+	public class PayPlayerPacket : Packet
+	{
+		public override string Type => "PAY_PLAYER";
+
+		public Player From { get; set; }
+
+		public Player To { get; set; }
+
+		public int Amount { get; set; }
+	}
+}
