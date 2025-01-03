25e9185c55692d7744343e25521f028ed8ab292d
Julia Tuka <julia.tuka@aon.at>
2025-01-03 23:16:14 +0100

    Changed Field Interface to have eventhandler

--- GameServer/GameServer/Models/IField.cs
+++ GameServer/GameServer/Models/IField.cs
@@ -2,8 +2,12 @@
 {
     public interface IField
     {
-        // When player lands on the field
-        public void LandOn(Player player);
+		event EventHandler<FieldEventArgs> FieldEventOccurred;
+
+		void RaiseEvent(string messageType, object? data);
+
+		// When player lands on the field
+		public void LandOn(Player player);
 
         // When player passes the field
         public void Pass(Player player);
