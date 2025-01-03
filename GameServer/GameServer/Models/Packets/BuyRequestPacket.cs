cab4a2107625db1f0b4b0f4f5f86aa3fc2068883
Julia Tuka <julia.tuka@aon.at>
2025-01-04 00:08:39 +0100

    Added Pay Packet for paying other player when landing on owned field

---
+++ GameServer/GameServer/Models/Packets/BuyRequestPacket.cs
@@ -0,0 +1,11 @@
+﻿namespace GameServer.Models.Packets
+{
+	public class BuyRequestPacket : Packet
+	{
+		public override string Type => "BUY_REQUEST";
+
+		public Player? Player { get; set; }
+
+		public IField? Field { get; set; }
+	}
+}
