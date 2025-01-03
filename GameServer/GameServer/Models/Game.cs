3ee235b38c9a2cc073e8b796606bc3ec0b20f0a2
kochmarvin <marvin.info@gmx.at>
2025-01-03 15:04:44 +0100

    :sparkles: changed Player to be null-able

--- GameServer/GameServer/Models/Game.cs
+++ GameServer/GameServer/Models/Game.cs
@@ -2,8 +2,8 @@
 {
     public class Game
     {
-        public Player CurrentMover;
-        public List<Player> Players { get; set; } = new List<Player>();
+        public Player? CurrentMover;
+        public List<Player> Players { get; set; } = [];
         public bool Started { get; set; }
 
         // TODO ganzer game state mit movement und dies und das
