e14085fe66eaf8c9c7297a572a648a12a16496ff
Julia Tuka <julia.tuka@aon.at>
2025-01-04 15:49:29 +0100

    Added Logic for landing on Site property

--- GameServer/GameServer/Models/GameLogic/Fields/Site.cs
+++ GameServer/GameServer/Models/GameLogic/Fields/Site.cs
@@ -1,4 +1,6 @@
-﻿namespace GameServer.Models
+﻿using GameServer.Models.Packets;
+
+namespace GameServer.Models
 {
 	public class Site : PropertyField
 	{
@@ -8,7 +10,45 @@
 
 		public override void LandOn(Player player)
 		{
-			// möglich sein, hat der owner dieser site alle felder von dieser gruppe 
+			if (Owner != null && Owner == player)
+			{
+				//RaiseEvent("INFO", $"{player.Name} ist auf der eigenen Station gelandet.");
+				return;
+			}
+
+			if (Owner != null && Owner != player)
+			{
+				int amount = 0;
+
+				if (Housecount > 0) // Ist mind. 1 Haus aus dem Grundstück, bleibt der Preis gleich 
+				{
+					amount = RentPrices[Housecount];
+				}
+				else if (Group.AllPropertiesOwnedBy(Owner)) // wenn alle im Besitz sind alle keine Häuser/Hotels vorhanden sind, verdoppelt sich die Rent
+				{
+					amount = RentPrices[0] * 2;
+				}
+				else // Gibts keine häuser und nicht alle Grundstücke sind im Besitz
+				{
+					amount = RentPrices[0]; 
+				}
+
+				if (player.TransferCurrency(Owner, amount))
+				{
+					RaiseEvent("PAY_PLAYER", new PayPlayerPacket() { From = player.Name, To = Owner.Name, Amount = amount, Successful = true });
+				}
+				else
+				{
+					RaiseEvent("PAY_PLAYER", new PayPlayerPacket() { From = player.Name, To = Owner.Name, Amount = amount, Successful = false });
+					// bancrupcy
+				}
+
+				return;
+			}
+
+			// wilst du zahlen? 
+			RaiseEvent("BUY_REQUEST", new BuyRequestPacket() { PlayerName = player.Name, FieldName = this.Name });
+
 		}
 
 		public override void Pass(Player player)
