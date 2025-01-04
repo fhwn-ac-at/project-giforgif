c5f19f28423df148f9a1bb528fe089adfaf24b36
DaPrix <prix.p.alexander@gmail.com>
2025-01-04 01:18:01 +0100

    Changed Type of From and To Player

--- GameServer/GameServer/Models/Packets/PayPlayerPacket.cs
+++ GameServer/GameServer/Models/Packets/PayPlayerPacket.cs
@@ -4,9 +4,9 @@
 	{
 		public override string Type => "PAY_PLAYER";
 
-		public Player From { get; set; }
+		public string From { get; set; }
 
-		public Player To { get; set; }
+		public string To { get; set; }
 
 		public int Amount { get; set; }
 	}
