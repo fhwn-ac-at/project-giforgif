72b4226b01ef71b8db3ae099d0879f1c59091292
DaPrix <prix.p.alexander@gmail.com>
2025-01-04 01:17:01 +0100

    Added StartAuctionPacket

---
+++ GameServer/GameServer/Models/Packets/StartAuctionPacket.cs
@@ -0,0 +1,9 @@
+﻿namespace GameServer.Models.Packets
+{
+    public class StartAuctionPacket : Packet
+    {
+        public override string Type => "AUCTION_START";
+
+        public string PropertyName { get; set; }
+    }
+}
