31ef80812275cf4c1342782af8285b0a374c4f6a
DaPrix <prix.p.alexander@gmail.com>
2025-01-04 01:16:21 +0100

    Added BuySuccessfulPacket

---
+++ GameServer/GameServer/Models/Packets/BuySuccessfulPacket.cs
@@ -0,0 +1,9 @@
+﻿namespace GameServer.Models.Packets
+{
+    public class BuySuccessfulPacket : Packet
+    {
+        public override string Type => "BUY_SUCESS";
+        public string PlayerName { get; set; }
+        public string PropertyName { get; set; }
+    }
+}
