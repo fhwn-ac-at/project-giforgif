383138038754c78a19ba3c083481058d23e1b16b
Julia Tuka <julia.tuka@aon.at>
2025-01-04 15:50:38 +0100

    Added logic for when player lands on Utility field and rolled dice property

--- GameServer/GameServer/Models/GameLogic/Fields/Utility.cs
+++ GameServer/GameServer/Models/GameLogic/Fields/Utility.cs
@@ -1,4 +1,6 @@
-﻿namespace GameServer.Models
+﻿using GameServer.Models.Packets;
+
+namespace GameServer.Models
 {
 	public class Utility : PropertyField
 	{
@@ -6,9 +8,45 @@
 		// if both are owned -> 10 time the dice 
 		//public int[] RentPrices { get; set; }
 
+		public int RolledDice { get; set; }
+
 		public override void LandOn(Player player)
 		{
-			throw new NotImplementedException();
+			if (Owner != null && Owner == player)
+			{
+				//RaiseEvent("INFO", $"{player.Name} ist auf der eigenen Utility gelandet.");
+				return;
+			}
+
+			if (Owner != null && Owner != player)
+			{
+				int ownedUtilities = Group.AmountOfWOwnedProperties(Owner);
+				int amount = 0;
+
+				if (ownedUtilities == 1)
+				{
+					amount = RolledDice * 4;
+				}
+				else if (ownedUtilities == 2) 
+				{
+					amount = RolledDice * 10;
+				}
+
+				if (player.TransferCurrency(Owner, amount))
+				{
+					RaiseEvent("PAY_PLAYER", new PayPlayerPacket() { From = player.Name, To = Owner.Name, Amount = amount, Successful = true });
+				}
+				else
+				{
+					RaiseEvent("PAY_PLAYER", new PayPlayerPacket() { From = player.Name, To = Owner.Name, Amount = amount, Successful = false });
+					// bancrupcy
+				}
+
+				return;
+			}
+
+			RaiseEvent("BUY_REQUEST", new BuyRequestPacket() { PlayerName = player.Name, FieldName = this.Name });
+
 		}
 
 		public override void Pass(Player player)
