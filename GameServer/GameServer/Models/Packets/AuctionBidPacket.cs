05e0cc898139a07f344c1e8a014a1b269757023a
DaPrix <prix.p.alexander@gmail.com>
2025-01-04 01:15:17 +0100

    Added AuctionResultPacket

---
+++ GameServer/GameServer/Models/Packets/AuctionBidPacket.cs
@@ -0,0 +1,10 @@
+﻿namespace GameServer.Models.Packets
+{
+    public class AuctionBidPacket : Packet
+    {
+        public override string Type => "AUCTION_BID";
+
+        public string PlayerName { get; set; }
+        public int Bid { get; set; }
+    }
+}
