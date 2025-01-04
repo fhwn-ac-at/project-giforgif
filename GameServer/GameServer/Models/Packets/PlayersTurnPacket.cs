8d848e731244e41d52c2139864c4734e5f15e2c9
Julia Tuka <julia.tuka@aon.at>
2025-01-04 17:17:18 +0100

    Added PlayesTurnPacket to indicate the new Players turn

---
+++ GameServer/GameServer/Models/Packets/PlayersTurnPacket.cs
@@ -0,0 +1,9 @@
+﻿namespace GameServer.Models.Packets
+{
+	public class PlayersTurnPacket : Packet
+	{
+		public override string Type => "PLAYERS_TURN";
+
+		public string? PlayerName { get; set; }
+	}
+}
