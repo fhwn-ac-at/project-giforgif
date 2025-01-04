13cbca8bcb5248883b03bf29a4f2740bf2a611a9
kochmarvin <marvin.info@gmx.at>
2025-01-04 17:15:51 +0100

    :zap: added new font and safelist styles

--- game-angular/tailwind.config.js
+++ game-angular/tailwind.config.js
@@ -1,10 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+  safelist: [
+    "border-blue-600",
+    "border-yellow-600",
+    "border-green-600",
+    "border-red-600",
+    "border-b-blue-600",
+    "border-b-yellow-600",
+    "border-b-green-600",
+    "border-b-red-600",
+    "border-r-blue-600",
+    "border-r-yellow-600",
+    "border-r-green-600",
+    "border-r-red-600",
+    "border-l-blue-600",
+    "border-l-yellow-600",
+    "border-l-green-600",
+    "border-l-red-600",
+    "border-t-blue-600",
+    "border-t-yellow-600",
+    "border-t-green-600",
+    "border-t-red-600",
+  ],
+};
