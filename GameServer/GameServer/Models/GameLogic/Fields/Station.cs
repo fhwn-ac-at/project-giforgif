ab6ce1c37e3f1fa23843f634f2849cb00f446136
Julia Tuka <julia.tuka@aon.at>
2025-01-04 15:50:03 +0100

    Added Logic for a player landing on Station property

--- GameServer/GameServer/Models/GameLogic/Fields/Station.cs
+++ GameServer/GameServer/Models/GameLogic/Fields/Station.cs
@@ -4,6 +4,7 @@ namespace GameServer.Models
 {
 	public class Station : PropertyField
 	{
+		// rent for 1, 2, 3 & 4 Stations owned 
 		public int[] RentPrices { get; set; }
 
 		public override void LandOn(Player player)
@@ -16,9 +17,19 @@ namespace GameServer.Models
 
 			if (Owner != null && Owner != player) 
 			{
-				int amount = 100;
-				player.TransferCurrency(Owner, amount); // TODO: price anpassen und häuser checken und schauen ob player zahlen kann
-				RaiseEvent("PAY_PLAYER", new PayPlayerPacket() { From = player.Name, To = Owner.Name, Amount = amount});
+				int amount = RentPrices[Group.AmountOfWOwnedProperties(Owner) - 1];
+
+				if (player.TransferCurrency(Owner, amount))
+				{
+					RaiseEvent("PAY_PLAYER", new PayPlayerPacket() { From = player.Name, To = Owner.Name, Amount = amount, Successful = true });
+				}
+				else
+				{
+					RaiseEvent("PAY_PLAYER", new PayPlayerPacket() { From = player.Name, To = Owner.Name, Amount = amount, Successful = false });
+					// bancrupcy
+				}
+
+				return;
 			}
 
 			// wilst du zahlen? 
