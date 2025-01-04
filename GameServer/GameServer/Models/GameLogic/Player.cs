70a99aef0e9c06ba946108d81cd61bb5ebd2754b
Julia Tuka <julia.tuka@aon.at>
2025-01-04 16:47:15 +0100

    Changed player class so they store there owned properties and cards also bankruptcy logic

--- GameServer/GameServer/Models/GameLogic/Player.cs
+++ GameServer/GameServer/Models/GameLogic/Player.cs
@@ -1,63 +1,110 @@
-﻿namespace GameServer.Models
+﻿using GameServer.Models.GameLogic;
+
+namespace GameServer.Models
 {
-    public class Player
-    {
-        // das ist noch nicht der richtige content
-        public string ConnectionId { get; set; }
-        public string Name { get; set; }
-        public int Currency { get; set; }
-        public IField CurrentPosition { get; set; }
+	public class Player
+	{
+		// das ist noch nicht der richtige content
+		public string ConnectionId { get; set; }
+		public string Name { get; set; }
+		public int Currency { get; set; }
+		public IField CurrentPosition { get; set; }
+		public List<PropertyField> Properties { get; set; } = new();
+		public List<Card> Cards { get; set; } = new();
 
-        public Player(string name, string connectionId)
-        {
-            this.Name = name;
-            this.ConnectionId = connectionId;
-        }
+		public Player(string name, string connectionId)
+		{
+			this.Name = name;
+			this.ConnectionId = connectionId;
+		}
 
-        public bool TransferCurrency(Player recipient, int amount)
-        {
-            if (this.Currency < amount)
-            {
-                return false;
-            }
+		public bool TransferCurrency(Player recipient, int amount)
+		{
+			if (this.Currency < amount)
+			{
+				return false;
+			}
 
-            this.Currency -= amount;
-            recipient.Currency += amount;
+			this.Currency -= amount;
+			recipient.Currency += amount;
 
-            return true;
-        }
+			return true;
+		}
 
-        public bool BuyCurrentField()
-        {
-            if (this.CurrentPosition != typeof(PropertyField))
-            {
-                return false;
-            }
+		//public bool BuyCurrentField()
+		//{
+		//    if (this.CurrentPosition != typeof(PropertyField))
+		//    {
+		//        return false;
+		//    }
 
-            PropertyField field = (PropertyField)this.CurrentPosition;
+		//    PropertyField field = (PropertyField)this.CurrentPosition;
 
-            if (this.Currency < field.BuyingPrice)
-            {
-                return false;
-            }
+		//    if (this.Currency < field.BuyingPrice)
+		//    {
+		//        return false;
+		//    }
 
-            this.Currency -= field.BuyingPrice;
-            field.Owner = this;
+		//    this.Currency -= field.BuyingPrice;
+		//    field.Owner = this;
 
-            return true;
-        }
+		//    return true;
+		//}
 
-        public bool BuyField(PropertyField field, int price)
-        {
-            if (this.Currency < price)
-            {
-                return false;
-            }
+		public bool BuyField(PropertyField field, int price)
+		{
+			if (this.Currency < price)
+			{
+				return false;
+			}
 
-            this.Currency -= price;
-            field.Owner = this;
+			this.Currency -= price;
+			field.Owner = this;
 
-            return true;
-        }
-    }
+			return true;
+		}
+
+		public void DeclareBankruptcyToPlayer(Player recipient)
+		{
+			recipient.Currency += Currency;
+			Currency = 0;
+
+			foreach (PropertyField property in Properties)
+			{
+				if (property is Site site)
+				{
+					int sellvalue = site.Housecount * (site.BuyingPrice / 2);
+					recipient.Currency += sellvalue;
+					site.Housecount = 0;
+				}
+
+				property.Owner = recipient;
+				recipient.Properties.Add(property);
+			}
+
+			Properties.Clear();
+
+			recipient.Cards.AddRange(Cards);
+			Cards.Clear();
+		}
+
+		public void DeclareBankruptcyToBank()
+		{
+			Currency = 0;
+
+			foreach (PropertyField property in Properties)
+			{
+				if (property is Site site)
+				{
+					site.Housecount = 0;
+				}
+
+				property.Owner = null;
+			}
+
+			Properties.Clear();
+
+			Cards.Clear(); // Or return do deck if there is only a specific number of cards, and cards have to be returned
+		}
+	}
 }
