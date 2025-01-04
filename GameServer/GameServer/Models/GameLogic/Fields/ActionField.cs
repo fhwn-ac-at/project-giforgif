9a46af6e82a5eb05c48885f3a4c01b385f01b32d
DaPrix <prix.p.alexander@gmail.com>
2025-01-04 01:13:42 +0100

    Added Name to IField

--- GameServer/GameServer/Models/GameLogic/Fields/ActionField.cs
+++ GameServer/GameServer/Models/GameLogic/Fields/ActionField.cs
@@ -1,13 +1,24 @@
-﻿namespace GameServer.Models
+﻿using GameServer.Models.Packets;
+
+namespace GameServer.Models
 {
     public class ActionField : IField
     {
+        public string Name { get; set; }
+
+        public event EventHandler<Packet> FieldEventOccurred;
+
         public void LandOn(Player player)
         {
             throw new NotImplementedException();
         }
 
         public void Pass(Player player)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RaiseEvent(string messageType, Packet data)
         {
             throw new NotImplementedException();
         }
