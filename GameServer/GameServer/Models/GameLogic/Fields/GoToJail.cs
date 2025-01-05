2d2a3be9e7e65af06dd70b83839898989fd193c5
Julia Tuka <julia.tuka@aon.at>
2025-01-05 14:15:34 +0100

    Added new GoToJail class as action field

---
+++ GameServer/GameServer/Models/GameLogic/Fields/GoToJail.cs
@@ -0,0 +1,10 @@
+﻿namespace GameServer.Models
+{
+	public class GoToJail : ActionField
+	{
+		public override void Accept(IFieldVisitor visitor, Player player, bool isLanding)
+		{
+			visitor.Visit(this, player, isLanding);
+		}
+	}
+}
