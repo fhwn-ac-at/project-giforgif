be6a0e6199ff43fd1fe2939cd3261a0f09bd0aae
Julia Tuka <julia.tuka@aon.at>
2025-01-05 14:12:34 +0100

    Added FieldVisitor

---
+++ GameServer/GameServer/Models/GameLogic/IFieldVisitor.cs
@@ -0,0 +1,12 @@
+﻿namespace GameServer.Models
+{
+	public interface IFieldVisitor
+	{
+		void Visit(Site site, Player player, bool isLanding);
+		void Visit(Station station, Player player, bool isLanding);
+		void Visit(Utility utility, Player player, bool isLanding);
+		void Visit(CommunityChest communityChest, Player player, bool isLanding);
+		void Visit(Chance chance, Player player, bool isLanding);
+		void Visit(GoToJail goToJail, Player player, bool isLanding);
+	}
+}
