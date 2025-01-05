63d1af09ed7a0270e9087bf1e76509a9a347ac18
Julia Tuka <julia.tuka@aon.at>
2025-01-05 16:28:59 +0100

    Added BuildingPrice and additional Methods for building houses

--- GameServer/GameServer/Models/GameLogic/Fields/Site.cs
+++ GameServer/GameServer/Models/GameLogic/Fields/Site.cs
@@ -1,16 +1,46 @@
 ﻿using GameServer.Models.Packets;
+using System.Text.Json;
 
 namespace GameServer.Models
 {
 	public class Site : PropertyField
 	{
 		public int Housecount { get; set; }
+
+		public int BuildingPrice { get; set; }
 		// Rent Prices for 0, 1, 2, 3, 4 houses and hotel
 		public int[] RentPrices { get; set; } = [];
 
 		public override void Accept(IFieldVisitor visitor, Player player, bool isLanding)
 		{
 			visitor.Visit(this, player, isLanding);
+		}
+
+		public bool CanBuildHouse(Player player)
+		{
+			if (Owner != player)
+				return false;
+
+			if (!Group.AllPropertiesOwnedBy(player)) 
+				return false;
+
+			int maxHousesInGroup = Group.Properties.Max(p => ((Site)p).Housecount);
+			int minHousesInGroup = Group.Properties.Min(p => ((Site)p).Housecount);
+
+			if (Housecount > minHousesInGroup)
+				return false;
+
+			return true;
+		}
+
+		public bool BuildHouse(Player player)
+		{
+			if (!CanBuildHouse(player)) 
+				return false;
+
+			player.DeductCurrency(BuildingPrice);
+			Housecount++;
+			return true;
 		}
 
 		//public override void LandOn(Player player)
