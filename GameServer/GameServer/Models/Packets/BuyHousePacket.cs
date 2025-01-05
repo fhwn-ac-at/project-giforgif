9f5f1d09b58e647ba2a34decfd043407274bfffa
Julia Tuka <julia.tuka@aon.at>
2025-01-05 16:31:41 +0100

    Added Packet for buying a house on a property

---
+++ GameServer/GameServer/Models/Packets/BuyHousePacket.cs
@@ -0,0 +1,9 @@
+﻿namespace GameServer.Models.Packets
+{
+	public class BuyHousePacket : Packet
+	{
+		public override string Type => "BUY_HOUSE";
+
+		public string? PropertyName { get; set; }
+	}
+}
