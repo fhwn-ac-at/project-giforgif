ac32ec9811702e6cddceec53ee9ca5a7796950db
Julia Tuka <julia.tuka@aon.at>
2025-01-05 21:42:11 +0100

    Added Auction bid update packets to notify fronend of new bid

---
+++ GameServer/GameServer/Models/Packets/Auction/AuctionBidUpdatePacket.cs
@@ -0,0 +1,11 @@
+﻿namespace GameServer.Models.Packets
+{
+	public class AuctionBidUpdatePacket : Packet
+	{
+		public override string Type => "AUCTION_UPDATE";
+
+		public int CurrentBid { get; set; }
+
+		public string? HighestBidderName { get; set; }
+	}
+}
