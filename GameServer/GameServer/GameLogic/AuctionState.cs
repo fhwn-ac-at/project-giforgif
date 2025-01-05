e03ab9e78a5367bfe881f4dc4d44f1656c20886d
Julia Tuka <julia.tuka@aon.at>
2025-01-05 21:41:29 +0100

    Added Auction state for handling action bids coming in

---
+++ GameServer/GameServer/GameLogic/AuctionState.cs
@@ -0,0 +1,56 @@
+﻿namespace GameServer.GameLogic
+{
+	public class AuctionState
+	{
+		public int FieldId { get; set; }
+		public Player HighestBidder { get; set; }
+		public int HighestBid { get; set; }
+		private Timer _timer;
+		private readonly int _timeout = 10000;
+
+		public event Action<AuctionState> AuctionEnded;
+
+		public AuctionState(int fieldId)
+		{
+			FieldId = fieldId;
+		}
+
+		public bool PlaceBid(Player player, int bidAmount)
+		{
+			lock (this)
+			{
+				if (bidAmount <= HighestBid)
+					return false;
+
+				if (!player.CanAfford(bidAmount))
+					return false;
+
+				HighestBid = bidAmount;
+				HighestBidder = player;
+
+				RestartTimer();
+
+				return true;
+			}
+		}
+
+		private void RestartTimer()
+		{
+			_timer.Change(_timeout, Timeout.Infinite);
+		}
+
+		public void StartTimer()
+		{
+			_timer = new Timer(OnAuctionTimeout, null, _timeout, Timeout.Infinite);
+		}
+
+		private void OnAuctionTimeout(object? state)
+		{
+			lock (this)
+			{
+				_timer.Dispose();
+				AuctionEnded?.Invoke(this);
+			}
+		}
+	}
+}
