7b40e2e003dc6cdfb207ce83db34bbd9480271fe
Julia Tuka <julia.tuka@aon.at>
2025-01-05 14:14:37 +0100

    Modified to have Field Visitor

--- GameServer/GameServer/Models/GameLogic/Fields/Station.cs
+++ GameServer/GameServer/Models/GameLogic/Fields/Station.cs
@@ -7,36 +7,41 @@ namespace GameServer.Models
 		// rent for 1, 2, 3 & 4 Stations owned 
 		public int[] RentPrices { get; set; } = [];
 
-		public override void LandOn(Player player)
+		public override void Accept(IFieldVisitor visitor, Player player, bool isLanding)
 		{
-			if (Owner != null && Owner == player)
-			{
-				return;
-			}
-
-			if (Owner != null && Owner != player) 
-			{
-				int amount = RentPrices[Group.AmountOfWOwnedProperties(Owner) - 1];
-
-				if (player.TransferCurrency(Owner, amount))
-				{
-					RaiseEvent("PAY_PLAYER", new PayPlayerPacket() { From = player.Name, To = Owner.Name, Amount = amount });
-				}
-				else
-				{
-					RaiseEvent("BANKRUPTCY", new BankruptcyPacket() { PlayerName = player.Name });
-					player.DeclareBankruptcyToPlayer(Owner);
-				}
-
-				return;
-			}
-
-			RaiseEvent("BUY_REQUEST", new BuyRequestPacket() { PlayerName = player.Name, FieldName = this.Name});
+			visitor.Visit(this, player, isLanding);
 		}
 
-		public override void Pass(Player player)
-		{
-			throw new NotImplementedException();
-		}
+		//public override void LandOn(Player player)
+		//{
+		//	if (Owner != null && Owner == player)
+		//	{
+		//		return;
+		//	}
+
+		//	if (Owner != null && Owner != player) 
+		//	{
+		//		int amount = RentPrices[Group.AmountOfWOwnedProperties(Owner) - 1];
+
+		//		if (player.TransferCurrency(Owner, amount))
+		//		{
+		//			RaiseEvent("PAY_PLAYER", new PayPlayerPacket() { From = player.Name, To = Owner.Name, Amount = amount });
+		//		}
+		//		else
+		//		{
+		//			RaiseEvent("BANKRUPTCY", new BankruptcyPacket() { PlayerName = player.Name });
+		//			player.DeclareBankruptcyToPlayer(Owner);
+		//		}
+
+		//		return;
+		//	}
+
+		//	RaiseEvent("BUY_REQUEST", new BuyRequestPacket() { PlayerName = player.Name, FieldName = this.Name});
+		//}
+
+		//public override void Pass(Player player)
+		//{
+		//	throw new NotImplementedException();
+		//}
 	}
 }
