6d4f63c3ad5bbbbf1eb6e5b380fe774681a8ee56
Julia Tuka <julia.tuka@aon.at>
2025-01-05 14:14:31 +0100

    Modified to have Field Visitor

--- GameServer/GameServer/Models/GameLogic/Fields/Utility.cs
+++ GameServer/GameServer/Models/GameLogic/Fields/Utility.cs
@@ -10,46 +10,51 @@ namespace GameServer.Models
 
 		public int RolledDice { get; set; }
 
-		public override void LandOn(Player player)
+		public override void Accept(IFieldVisitor visitor, Player player, bool isLanding)
 		{
-			if (Owner != null && Owner == player)
-			{
-				return;
-			}
-
-			if (Owner != null && Owner != player)
-			{
-				int ownedUtilities = Group.AmountOfWOwnedProperties(Owner);
-				int amount = 0;
-
-				if (ownedUtilities == 1)
-				{
-					amount = RolledDice * 4;
-				}
-				else if (ownedUtilities == 2) 
-				{
-					amount = RolledDice * 10;
-				}
-
-				if (player.TransferCurrency(Owner, amount))
-				{
-					RaiseEvent("PAY_PLAYER", new PayPlayerPacket() { From = player.Name, To = Owner.Name, Amount = amount});
-				}
-				else
-				{
-					RaiseEvent("BANKRUPTCY", new BankruptcyPacket() { PlayerName = player.Name });
-					player.DeclareBankruptcyToPlayer(Owner);
-				}
-
-				return;
-			}
-
-			RaiseEvent("BUY_REQUEST", new BuyRequestPacket() { PlayerName = player.Name, FieldName = this.Name });
+			visitor.Visit(this, player, isLanding);
 		}
 
-		public override void Pass(Player player)
-		{
-			throw new NotImplementedException();
-		}
+		//public override void LandOn(Player player)
+		//{
+		//	if (Owner != null && Owner == player)
+		//	{
+		//		return;
+		//	}
+
+		//	if (Owner != null && Owner != player)
+		//	{
+		//		int ownedUtilities = Group.AmountOfWOwnedProperties(Owner);
+		//		int amount = 0;
+
+		//		if (ownedUtilities == 1)
+		//		{
+		//			amount = RolledDice * 4;
+		//		}
+		//		else if (ownedUtilities == 2) 
+		//		{
+		//			amount = RolledDice * 10;
+		//		}
+
+		//		if (player.TransferCurrency(Owner, amount))
+		//		{
+		//			RaiseEvent("PAY_PLAYER", new PayPlayerPacket() { From = player.Name, To = Owner.Name, Amount = amount});
+		//		}
+		//		else
+		//		{
+		//			RaiseEvent("BANKRUPTCY", new BankruptcyPacket() { PlayerName = player.Name });
+		//			player.DeclareBankruptcyToPlayer(Owner);
+		//		}
+
+		//		return;
+		//	}
+
+		//	RaiseEvent("BUY_REQUEST", new BuyRequestPacket() { PlayerName = player.Name, FieldName = this.Name });
+		//}
+
+		//public override void Pass(Player player)
+		//{
+		//	throw new NotImplementedException();
+		//}
 	}
 }
