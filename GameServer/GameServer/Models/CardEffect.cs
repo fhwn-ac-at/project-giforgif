92cfe507094b57cdcb4ce4d7e1887e990c33f131
DaPrix <prix.p.alexander@gmail.com>
2025-01-06 17:14:45 +0100

    Added new CardEffect Subclass

---
+++ GameServer/GameServer/Models/CardEffect.cs
@@ -0,0 +1,18 @@
+﻿using GameServer.GameLogic;
+
+namespace GameServer.Models
+{
+    public class CardEffect
+    {
+        public bool IsInstant { get; set; }
+
+        public Action<Player> Effect { get; set; }
+
+        public CardEffect(bool isInstant, Action<Player> effect)
+        {
+            IsInstant = isInstant;
+            Effect = effect;
+        }
+
+    }
+}
