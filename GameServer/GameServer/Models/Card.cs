126df410365fba99e4cdd3586a5df3ee50da742f
DaPrix <prix.p.alexander@gmail.com>
2025-01-06 16:03:28 +0100

    Added ID to card

--- GameServer/GameServer/Models/Card.cs
+++ GameServer/GameServer/Models/Card.cs
@@ -4,6 +4,8 @@ namespace GameServer.Models
 {
 	public class Card // evtl. unterscheidung zwischen Karten die man besitzen kann und nicht besitzen kann
 	{
+		public int Id { get; set; }
+
 		public string? Name { get; set; }
 
 		public string? Description { get; set; }
