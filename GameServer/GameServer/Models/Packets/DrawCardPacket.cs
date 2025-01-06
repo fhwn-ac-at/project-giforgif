933f0cf9d414603c87633c8790e570eda9c2b483
DaPrix <prix.p.alexander@gmail.com>
2025-01-06 17:14:16 +0100

    Added new Packet for notifying of a card draw

---
+++ GameServer/GameServer/Models/Packets/DrawCardPacket.cs
@@ -0,0 +1,19 @@
+﻿namespace GameServer.Models.Packets
+{
+    public class DrawCardPacket : Packet
+    {
+        public override string Type => "DRAW_CARD";
+
+        public string PlayerName { get; set; }
+
+        public string CardName { get; set; }
+
+        public string CardDescription { get; set; }
+        public DrawCardPacket(string playerName, string cardName, string cardDescription)
+        {
+            PlayerName = playerName;
+            CardName = cardName;
+            CardDescription = cardDescription;
+        }
+    }
+}
