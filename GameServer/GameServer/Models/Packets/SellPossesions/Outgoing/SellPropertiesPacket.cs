536f1b15c5857160067d54e8265655ccba3a82a3
DaPrix <prix.p.alexander@gmail.com>
2025-01-12 18:42:57 +0100

    Added packet to notify player he needs to sell

---
+++ GameServer/GameServer/Models/Packets/SellPossesions/Outgoing/SellPropertiesPacket.cs
@@ -0,0 +1,8 @@
+﻿namespace GameServer.Models.Packets
+{
+    public class SellPropertiesPacket : Packet
+    {
+        public override string Type => "SELL_PROPERTIES";
+        public int Amount { get; set; } // Amount of money player has to sell in order to pay
+    }
+}
