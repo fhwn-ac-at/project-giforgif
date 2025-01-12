af3937225d8028ff101e2df695561a5262a13cd8
DaPrix <prix.p.alexander@gmail.com>
2025-01-12 18:42:34 +0100

    Added incoming Packet for selling Property

---
+++ GameServer/GameServer/Models/Packets/SellPossesions/Incoming/SellHousePacket.cs
@@ -0,0 +1,9 @@
+﻿namespace GameServer.Models.Packets
+{
+    public class SellHousePacket : Packet
+    {
+        public override string Type => "SELL_HOUSE";
+
+        public int FieldId { get; set; }
+    }
+}
