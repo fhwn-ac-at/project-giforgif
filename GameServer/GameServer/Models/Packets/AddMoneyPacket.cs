dca223e40693220d8aa86359ac758c064e802c3e
DaPrix <prix.p.alexander@gmail.com>
2025-01-12 16:22:38 +0100

    Added AddMoneyPacket

---
+++ GameServer/GameServer/Models/Packets/AddMoneyPacket.cs
@@ -0,0 +1,11 @@
+﻿namespace GameServer.Models.Packets
+{
+    public class AddMoneyPacket : Packet
+    {
+        public override string Type => "ADD_MONEY";
+
+        public string? PlayerName { get; set; }
+        public int Amount { get; set; }
+        public string? Description { get; set; }
+    }
+}
