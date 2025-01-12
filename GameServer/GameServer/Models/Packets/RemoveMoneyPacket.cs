957877701a34d586212e1b7f4d1022f7467a3a7b
DaPrix <prix.p.alexander@gmail.com>
2025-01-12 16:22:46 +0100

    Added RemoveMoneyPacket

---
+++ GameServer/GameServer/Models/Packets/RemoveMoneyPacket.cs
@@ -0,0 +1,11 @@
+﻿namespace GameServer.Models.Packets
+{
+    public class RemoveMoneyPacket : Packet
+    {
+        public override string Type => "REMOVE_MONEY";
+
+        public string? PlayerName { get; set; }
+        public int Amount { get; set; }
+        public string? Description { get; set; }
+    }
+}
