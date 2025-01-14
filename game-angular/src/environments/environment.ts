7406d5b3bdda21cc962fab82a32e706060e495a5
kochmarvin <marvin.info@gmx.at>
2025-01-15 00:31:39 +0100

    :whale: dockerize game

--- game-angular/src/environments/environment.ts
+++ game-angular/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:32769',
+  apiUrl: 'http://ksc84sgwwgcs4kooc888cs8w.116.203.80.175.sslip.io:3000',
   clientId:
     '428162796477-hetnc1panp43ctufhg6f8no47dr27a1u.apps.googleusercontent.com',
 };
