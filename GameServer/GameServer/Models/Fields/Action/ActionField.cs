89bf3315002112e81ce7d640effcb1afa07f1ee5
DaPrix <prix.p.alexander@gmail.com>
2025-01-15 21:52:27 +0100

    Removed Comments

--- GameServer/GameServer/Models/Fields/Action/ActionField.cs
+++ GameServer/GameServer/Models/Fields/Action/ActionField.cs
@@ -12,16 +12,6 @@ namespace GameServer.Models.Fields
 
         public abstract void Accept(IFieldVisitor visitor, Player player, bool isLanding);
 
-		//public void LandOn(Player player)
-  //      {
-  //          throw new NotImplementedException();
-  //      }
-
-  //      public void Pass(Player player)
-  //      {
-  //          throw new NotImplementedException();
-  //      }
-
         public void RaiseEvent(string messageType, Packet data)
         {
 			FieldEventOccurred?.Invoke(this, data);
