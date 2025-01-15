4ba987a1efacd9197e2818fb46bca2c58b4e61d0
kochmarvin <marvin.info@gmx.at>
2025-01-15 01:13:35 +0100

    :construction: port testing

--- GameServer/GameServer/Program.cs
+++ GameServer/GameServer/Program.cs
@@ -7,6 +7,8 @@ using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.UseUrls("http://0.0.0.0:3000");
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<DatabaseContext>(options =>
