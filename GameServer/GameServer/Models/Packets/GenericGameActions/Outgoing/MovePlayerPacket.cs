46f572d8d91320262fd51085e47cbb666e115147
DaPrix <prix.p.alexander@gmail.com>
2025-01-15 18:34:00 +0100

    Added Events to Cards

---
+++ GameServer/GameServer/Models/Packets/GenericGameActions/Outgoing/MovePlayerPacket.cs
@@ -0,0 +1,9 @@
+﻿namespace GameServer.Models.Packets
+{
+    public class MovePlayerPacket : Packet
+    {
+        public override string Type => "MOVE_PLAYER";
+        public string? PlayerName { get; set; }
+        public int FieldId { get; set; }
+    }
+}
