195327b564274db10ddab2eab0615fb78ca82980
DaPrix <prix.p.alexander@gmail.com>
2025-01-15 21:53:33 +0100

    Added new Packet for when god gets activated

---
+++ GameServer/GameServer/Models/Packets/Gods/Outgoing/PoliticActivatedPacket.cs
@@ -0,0 +1,9 @@
+﻿namespace GameServer.Models.Packets
+{
+    public class PoliticActivatedPacket : Packet
+    {
+        public override string Type => "NEW_POLITIC";
+
+        public int PoliticId { get; set; }
+    }
+}
